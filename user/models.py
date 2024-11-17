e5030de67e54d1a2a2d0067686b6cd883e7b6ba5
Umutoni-Rita <umurita37@gmail.com>
2024-11-17 17:51:44 +0200

    Login

--- user/models.py
+++ user/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
 
     def __str__(self):
--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            # Log the user in after registration
+            user = authenticate(username=new_user.username, password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+            return redirect('dashboard')  # Redirect to some page after successful registration
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
